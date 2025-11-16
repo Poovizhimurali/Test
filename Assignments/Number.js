ab1eff382e13129ee4b2b5e22c8c074d602fd691
poovizhimurali <poovizhiuday@gmail.com>
2025-11-16 12:05:39 +0530

    commit

--- Assignments/Number.js
+++
@@ -1,9 +0,0 @@
-// Step 1: Create a variable to store a number
-let number = 79
-if (number > 0) {
-    console.log("Positive");
-} else if (number < 0) {
-    console.log("Negative");
-} else {
-    console.log("Zero");
-}
